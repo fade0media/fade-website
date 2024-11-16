346297601080c793e0530c68dec111e2270de1c6
sanjana-bhat-2002 <asanjanabbhat@gmail.com>
2024-11-17 00:06:14 +0530

    updated logo font

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,20 +1,20 @@
-import localFont from 'next/font/local';
+// import localFont from 'next/font/local';
 import React from 'react';
-import { cn } from "@/lib/utils"; // Ensure cn is imported if it's available
+import { cn } from "@/lib/utils"; 
 import Link from 'next/link';
 
-const ibrand = localFont({
-  src: [
-    {
-      path: '../../app/fonts/ibrand.woff',
-      style: 'normal',
-    },
-  ],
-});
+// const ibrand = localFont({
+//   src: [
+//     {
+//       path: '../../app/fonts/ibrand.woff',
+//       style: 'normal',
+//     },
+//   ],
+// });
 
 const Logo = () => {
   return (
-    <Link href="/" className={cn(ibrand.className, "text-white text-3xl")}>
+    <Link href="/" className={("text-white text-2xl font-semibold")}>
       fade.
     </Link>
   );
